4e778d6e4e0c2807bdbc8fed70af3fbf7395465b
gisellealmario <gisellealmario@gmail.com>
2023-11-20 19:12:17 +0000

    added helper to get questions for quiz

---
+++ db/test/helpersTest.js
@@ -0,0 +1,12 @@
+
+const { getQuestionsForQuiz } = require('../queries/helpers');
+
+
+const quizId = 1; // Replace with the actual quiz ID you want to retrieve questions for
+getQuestionsForQuiz(quizId)
+  .then(questions => {
+    console.log('Questions for quiz:', questions);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
