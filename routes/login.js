e6b276343c95ceb0d5f929bda7cac69a723bfbfb
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-11-21 07:11:30 -0700

    Add login route for login bypass

---
+++ routes/login.js
@@ -0,0 +1,18 @@
+/*
+ * All routes for Login bypass are defined here
+ * Since this file is loaded in server.js into login,
+ *   these routes are mounted onto /login
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const db = require('../db/connection');
+
+//bypass login http;//localhost:8080/login/1
+router.get('/:id', (req, res) => {
+  res.cookie('user_id', req.params.id);
+  return res.redirect('../index');
+});
+
+module.exports = router;
