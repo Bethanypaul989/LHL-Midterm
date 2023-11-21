e292f8b6e9cc8240bae590246247901b3ead4a67
gisellealmario <gisellealmario@gmail.com>
2023-11-21 18:31:25 +0000

    provided get types of quizzes as helper

--- db/queries/getQuestionsForQuiz.js
+++ db/queries/getQuestionsForQuiz.js
@@ -4,7 +4,7 @@ const db = require('../connection');
 // Helper function to get questions by quiz category
 const getQuestionsForQuiz = (quizId) => {
   const query = {
-    text: 'SELECT * FROM questions JOIN quizzes ON questions.quizzes_id = quizzes.id WHERE quizzes.id = $1',
+    text: 'SELECT * FROM questions JOIN quizzes ON questions.quizzes_id = quizzes.id WHERE quizzes.id = $1 ORDER BY random() LIMIT 3;',
     values: [quizId],
   };
 
@@ -19,3 +19,14 @@ const getQuestionsForQuiz = (quizId) => {
 };
 
 module.exports = { getQuestionsForQuiz };
+
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+//   })
+// });
