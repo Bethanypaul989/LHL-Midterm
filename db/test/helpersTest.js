64c69c9df9646c5e192269d4f243d2390c64264e
gisellealmario <gisellealmario@gmail.com>
2023-11-22 02:33:52 +0000

    added helper to get types of quizzes

--- db/test/helpersTest.js
+++ db/test/helpersTest.js
@@ -1,12 +1,21 @@
 
-const { getQuestionsForQuiz } = require('../queries/getQuestionsForQuiz');
+const { getTypesofQuizzes } = require('../queries/getTypesofQuizzes');
 
 
-const quizId = 1; // Replace with the actual quiz ID you want to retrieve questions for
-getQuestionsForQuiz(quizId)
-  .then(questions => {
-    console.log('Questions for quiz:', questions);
+// const quizId = 2; // Replace with the actual quiz ID you want to retrieve questions for
+// getQuestionsForQuiz(quizId)
+//   .then(questions => {
+//     console.log('Questions for quiz:', questions);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+
+getTypesofQuizzes()
+  .then(quizzes => {
+    console.log('Types of quizzes:', quizzes);
   })
   .catch(error => {
-    console.error('Error:', error);
-  });
+    console.log('error:', error);
+  })
