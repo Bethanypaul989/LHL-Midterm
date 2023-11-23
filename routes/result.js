27b5e1b1d4c701b08c8e16713aa9bf6d65f8ed99
Bethanypaul989 <slayy.mayaa188@gmail.com>
2023-11-22 21:57:28 -0700

    fixed routes result.js had a typo

--- routes/result.js
+++ routes/result.js
@@ -12,6 +12,9 @@ const resultQuery = require('../db/queries/retrievingQuizResult');
 // get user quiz attempt result - url can be shared - path for front end to hit /result/:id
 router.get('/:id', (req, res) => {
   const resultId = req.params.id;
-
+  /*Implement helper function that retrieves quiz result from database based on result id*/
+  /*Render result EJS template with quiz result passed in as a variable*/
+  return res.render('quizResults' /*need to add template vars - quiz list from database*/);
+});
 
 module.exports = router;
